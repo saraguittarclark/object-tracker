1e6201f22b5c1ee761fcd149e604c478ae70401d
Sara Clark <sara.guittar@gmail.com>
2016-11-18 11:19:22 -0500

    reworked to reflect view/create focus

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,40 +8,55 @@
 
         $stateProvider
 
-            .state('search', {
-                url: '/search',
-                templateUrl: '/templates/search.html',
+            .state('create', {
+                url: '/create',
+                templateUrl: '/templates/create.html',
                 controller: 'ResultCtrl as result'
             })
+
+            .state('create.antenna', {
+                url:'/antenna',
+                templateUrl: '/templates/antenna.html'
+            })
          	
-         	.state('search.receiver', {
+         	.state('create.receiver', {
          		url: '/receiver',
                 controller: 'ReceiverCtrl as receiver',
          		templateUrl: '/templates/receiver.html'
          	})
 
-            .state('search.transmitter-default', {
+            .state('create.transmitter-default', {
                 url:'/transmitter-default',
                 controller: 'TransmitterDCtrl as transmitterDefault',
                 templateUrl: '/templates/transmitter-default.html'
             })
 
-            .state('search.transmitter-eirp', {
+            .state('create.transmitter-eirp', {
                 url:'/transmitter-eirp',
                 controller:'TransmitterECtrl as transmitterEirp',
                 templateUrl: '/templates/transmitter-eirp.html'
             }) 
 
-            .state('search.platform-satellite', {
+            .state('create.platform-satellite', {
                 url: '/platform-satellite',
                 controller: 'PlatformTLECtrl as platformSatellite',
                 templateUrl: '/templates/platform-satellite.html'
             })
 
-            .state('search.platform-static', {
+            .state('create.platform-static', {
                 url: '/platform-static',
                 controller: 'PlatformStaticCtrl as platformStatic',
                 templateUrl: '/templates/platform-static.html'
+            })
+
+            .state('create.tle', {
+                url:'/tle',
+                templateUrl:'/templates/tle.html'
+            })
+
+            .state('create.odo', {
+                url:'/odo',
+                templateUrl:'/templates/odo.html'
             })
 
             .state('result', {
